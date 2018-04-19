34453a8b5df3922bd2c2095ee217328ef407383c
AF1989 <AF1989@ua.fm>
2018-04-19 15:58:54 +0300

    first commit for JavaSlovo

---
+++ slovo/slovo.java
@@ -0,0 +1,27 @@
+package slovo;
+
+import java.util.Scanner;
+
+public class slovo {
+
+	public static void main(String[] args) {
+		
+		String word;
+		Scanner inputDevice = new Scanner(System.in);
+		System.out.print("Введіть слово: ");
+		word = inputDevice.next();
+		char j = word.charAt(0);
+		char a = word.charAt(1);
+		char v = word.charAt(2);
+		char a2 = word.charAt(3);
+		System.out.println("     "+j+"       "+a+"    "+v+"       "+v+"   "+a);
+		System.out.println("     "+j+"      "+a+" "+a+"    "+v+"     "+v+"   "+a+" "+a);
+		System.out.println("     "+j+"     "+a+"   "+a+"    "+v+"   "+v+"   "+a+"   "+a);
+		System.out.println(j+"    "+j+"    "+a+a+a+a+a+a+a+"    "+v+" "+v+"   "+a+a+a+a+a+a+a);
+		System.out.println(j+""+j+j+j+j+j+"   "+a+"       "+a+"    "+v+"   "+a+"       "+a);
+		
+		
+
+	}
+
+}
